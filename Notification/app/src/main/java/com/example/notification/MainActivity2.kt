2d1e1c4e39f42f95850d953305dd886f40ce2fcf
MuhammadFaheemKhan2004 <faheemniazi2004@gmail.com>
2025-04-11 18:33:45 +0500

    notifucations completed

---
+++ Notification/app/src/main/java/com/example/notification/MainActivity2.kt
@@ -0,0 +1,21 @@
+package com.example.notification
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notification.databinding.ActivityMain2Binding
+import com.example.notification.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.setText(intent.getStringExtra("DATA_REC"))
+
+    }
+}
